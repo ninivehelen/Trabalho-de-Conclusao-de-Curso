68f4765d33bc54bfd3bab79d593d9477f76ba190
ninivehelen <ninivehelen@gmail.com>
2025-04-08 15:43:37 -0300

    Criação do dicionário para classificar as dimensões e inconsistencias

--- analise_qualidade/main.py
+++ analise_qualidade/main.py
@@ -7,28 +7,17 @@ diretorio = 'inconsistencias'
 def dimensao_busca(nome_inconsistencia):
     dimensao_dicionario =  {
     'vazio'  :  'Completude',
-    'X'  :  'Conformidade',
-    'X'  :  'Exatidão',
+    'vazio_1': 'Completude/Exatidão',
+    'X'  :  'Conformidade/Exatidão',
     'caracter_especial'  :  'Conformidade',
+    'caracter_especial_1'  :  'Conformidade/Exatidão',
     'dt_cadastro_aluno_sistema_inferior' :'Consistência',
     'dt_cadastro_ciclo_inferior': 'Consistência',
     'tipo_oferta_diferente_hifen': 'Consistência'
     }
     return dimensao_dicionario[nome_inconsistencia]
 
-def classfica_dimensoes_exatidao(df_dados):
-    lista_linhas = ['sg_sexo']
-    print("classificando inconsistências para adicionar a dimensão afetada.")
-    dimensao = []
-    for index, linha in df_dados.iterrows():
-        if linha.coluna in lista_linhas:
-            nome_inconsistencia = linha.tipo_inconsistencia
-            dimensao.append(dimensao_busca(nome_inconsistencia))
-    df_dados['Dimensão'] = dimensao
-    df_dados.to_csv(diretorio + '/base1_inconsistencias_unido_classificado.csv', sep=',', encoding="utf-8", index=False)
-    print('arquivo salvo com a dimensão classificada')
-
-def classfica_dimensoes_completude(df_dados):
+def classfica_dimensoes(df_dados):
     lista_linhas = ['sg_sexo']
     print("classificando inconsistências para adicionar a dimensão afetada.")
     dimensao = []
@@ -38,15 +27,13 @@ def classfica_dimensoes_completude(df_dados):
             dimensao.append(dimensao_busca(nome_inconsistencia))
     df_dados['Dimensão'] = dimensao
     salvar_classificacao(df_dados)
-   
+
 def salvar_classificacao(df_dados):
     df_dados.to_csv(diretorio + '/base1_inconsistencias_unido_classificado.csv', sep=',', encoding="utf-8", index=False)
     print('arquivo salvo com a dimensão classificada')
 
-
 if __name__ == "__main__":
-     df_dados = pd.read_csv('inconsistencias/base_dados_1_valida_sexo_aluno_sg_sexo_07_04_2025-15_56.csv', sep=',',encoding='utf-8')
-     df_dados = df_dados.replace(np.nan, 'vazio')
-     total_linhas_dados = df_dados.shape[0]
-     classfica_dimensoes_completude(df_dados)
+    # unir_arquivos()
+     df_dados = pd.read_csv('inconsistencias/base1_inconsistencias_unido.csv', sep=',',encoding='utf-8')
+     classfica_dimensoes(df_dados)
     
--- analise_qualidade/unir_inconsistencias.py
+++ analise_qualidade/unir_inconsistencias.py
@@ -1,14 +1,15 @@
 import glob
 import os 
 import pandas as pd 
-nome = '/base1_inconsistencias_unido.csv'
+import numpy as np 
 diretorio = 'inconsistencias'
+
 def unir_arquivos():
-    arquivos_csv = glob.glob(os.path.join(diretorio+ "/base_dados_1_*.csv")) 
+    arquivos_csv = glob.glob(os.path.join(diretorio+ "/base_dados_*.csv")) 
     base_dados = pd.DataFrame()
     for arquivo in arquivos_csv:
-        df_temp = pd.read_csv(arquivo, sep=',',encoding='utf-8', keep_default_na = False)
-        base_dados = pd.concat([df_temp,base_dados])
-        print("Unindo csv")
-        base_dados.to_csv(diretorio+'/base1_inconsistencias_unido.csv', sep=',',encoding="utf-8",index=False)
+        df_temp = pd.read_csv(arquivo, sep=',',encoding='utf-8')
+        base_dados  = pd.concat([df_temp,base_dados])
+    df_dados = df_dados.replace(np.nan, 'vazio')
+    df_dados.to_csv(diretorio+'/base1_inconsistencias_unido.csv', sep=',',encoding="utf-8",index=False)
     print("CSV com todas as inconsistências criado")
